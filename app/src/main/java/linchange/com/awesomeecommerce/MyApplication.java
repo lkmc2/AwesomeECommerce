a26ee2fee0dbfc0fb1a6c13f38623a343932e872
lkmc2 <lkmc2@163.com>
2017-12-27 14:07:40 +0800

    添加MyApplication

---
+++ app/src/main/java/linchange/com/awesomeecommerce/MyApplication.java
@@ -0,0 +1,17 @@
+package linchange.com.awesomeecommerce;
+
+import android.app.Application;
+
+/**
+ * Created by lkmc2 on 2017/12/27.
+ * 全局应用
+ */
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+
+    }
+}
