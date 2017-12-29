cd444365d5c61bb2c312809d7de78e6ebf5e951b
lkmc2 <lkmc2@163.com>
2017-12-29 12:33:19 +0800

    添加网络请求方式枚举

---
+++ core/src/main/java/linchange/com/net/HttpMethod.java
@@ -0,0 +1,16 @@
+package linchange.com.net;
+
+/**
+ * Created by lkmc2 on 2017/12/29.
+ * 网络请求方式枚举
+ */
+
+public enum HttpMethod {
+    GET, //get方式请求
+    POST, //post方式请求
+    POST_RAW, //post原生数据方式请求
+    PUT, //put存入方式请求
+    PUT_RAW, //put存入原生数据方式请求
+    DELETE, //delete删除方式请求
+    UPLOAD //upload上传方式请求
+}
